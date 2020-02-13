1ecd826e50991363506e271dc0f7f6e7f948ebfe
Ignacio Varela <ivarela@nacionservicios.com.ar>
2020-02-13 16:54:11 -0300

    Modifico variable salt para que sea final static y no tener problemas con los scopes de spring boot

--- src/main/java/santander/tecnologia/challenge/service/user/UserService.java
+++ src/main/java/santander/tecnologia/challenge/service/user/UserService.java
@@ -1,10 +1,7 @@
 package santander.tecnologia.challenge.service.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCrypt;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder.BCryptVersion;
 import org.springframework.stereotype.Service;
 
 import santander.tecnologia.challenge.domain.User;
@@ -12,15 +9,12 @@ import santander.tecnologia.challenge.exception.UserException;
 import santander.tecnologia.challenge.persistence.UserRepository;
 
 @Service
-@ComponentScan(basePackages = { "org.baeldung.security" })
-@EnableWebSecurity
 public class UserService {
 	
 	@Autowired
 	private UserRepository userRepository;
 	
-	private String salt = null;
-	
+	private final static String  salt = "$2a$04$IwmFubEGW8Nw.majttflsO";
 	
 	public User getUser(String username, String password) throws Exception {
 		
@@ -38,12 +32,8 @@ public class UserService {
 	
 	
 	public String encode(CharSequence rawPassword) {
-		if(salt == null) {
-			salt =  BCrypt.gensalt(BCryptVersion.$2A.getVersion(), 4);
-		}
 		return BCrypt.hashpw(rawPassword.toString(), salt);
 	}
 	
-	 
 
 }
