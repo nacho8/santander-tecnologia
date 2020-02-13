45abcd5b3df9836b3b0f14ce8254f40655861237
Ignacio Varela <ivarela@nacionservicios.com.ar>
2020-02-13 12:02:02 -0300

    Agrego parte de JWT/ROLES para segurizar los servicios!

--- src/main/java/santander/tecnologia/challenge/security/WebSecurityConfig.java
+++ src/main/java/santander/tecnologia/challenge/security/WebSecurityConfig.java
@@ -15,6 +15,7 @@ import santander.tecnologia.challenge.security.jwt.JWTAuthorizationFilter;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable()
